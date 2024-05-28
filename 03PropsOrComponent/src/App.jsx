e9578a6019bb231f6e4e96fc48aa7b48854b3e62
Harishsemwal <harishsemwal581@gmail.com>
2024-05-28 10:00:43 +0530

    commit
    commit

---
+++ 03PropsOrComponent/src/App.jsx
@@ -0,0 +1,12 @@
+// function Car(){
+//   return(
+//       <h1>This is the Best Car...</h1>
+//   )
+// }
+function Garbage(){
+  return (
+    <h1>Hello I am Harish...</h1>
+  )
+}
+
+export default Garbage;
---
+++ 03PropsOrComponent/src/Car.jsx
@@ -0,0 +1,6 @@
+function Car(){
+    return(
+        <h1>This is the Best Car...</h1>
+    )
+}
+export default Car;
